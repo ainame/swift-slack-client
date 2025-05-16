770817be0a3ae04c8d0ae7d605ca496153307bb8
ainame <s.namai.09@gmail.com>
2025-05-16 11:37:51 +0100

    Implement Slack actor

---
+++ Sources/SlackClient/Slack.swift
@@ -0,0 +1,91 @@
+import Foundation
+import OpenAPIRuntime
+import HTTPTypes
+
+public actor Slack {
+    private let _client: Client
+    private var requestConfigurationMiddleware: RequestConfigurationMiddlware
+
+    public var client: APIProtocol {
+        _client
+    }
+
+    public init(
+        serverURL: URL = URL(string: "https://slack.com/api")!,
+        transport: any ClientTransport,
+        middlewares: [any ClientMiddleware] = [],
+        configuration: ClientConfiguration = .init()
+    ) {
+        requestConfigurationMiddleware = RequestConfigurationMiddlware(configuration: configuration)
+        _client = Client(
+            serverURL: serverURL,
+            transport: transport,
+            middlewares: middlewares + [requestConfigurationMiddleware]
+        )
+    }
+
+    public func setAccessToken(_ value: String) async {
+        await requestConfigurationMiddleware.setAccessToken(value)
+    }
+}
+
+extension Slack {
+    public struct ClientConfiguration: Sendable {
+        public var userAgent: String?
+        public var accessToken: String?
+
+        public init(
+            userAgent: String? = nil,
+            accessToken: String? = nil
+        ) {
+            self.userAgent = userAgent
+            self.accessToken = accessToken
+        }
+    }
+}
+
+
+extension Slack {
+    private actor RequestConfigurationMiddlware: ClientMiddleware {
+        private var additionalHeaderFields: HTTPFields = .init()
+
+        private var configuration: ClientConfiguration {
+            didSet {
+                self.additionalHeaderFields = Self.updateAdditionalHeaders(configuration)
+            }
+        }
+
+        init(configuration: ClientConfiguration) {
+            self.configuration = configuration
+            self.additionalHeaderFields = Self.updateAdditionalHeaders(configuration)
+        }
+
+        static func updateAdditionalHeaders(_ configuration: ClientConfiguration) -> HTTPFields {
+            var additionalHeaderFields = HTTPFields()
+            additionalHeaderFields[.userAgent] = configuration.userAgent
+            if let accessToken = configuration.accessToken {
+                additionalHeaderFields[.authorization] = "Bearer \(accessToken)"
+            }
+            return additionalHeaderFields
+        }
+
+        func setAccessToken(_ value: String) {
+            configuration.accessToken = value
+        }
+
+        func intercept(
+            _ request: HTTPRequest,
+            body: HTTPBody?,
+            baseURL: URL,
+            operationID: String,
+            next: (HTTPRequest, HTTPBody?, URL) async throws -> (HTTPResponse, HTTPBody?)
+        ) async throws -> (
+            HTTPResponse,
+            HTTPBody?
+        ) {
+            var req = request
+            req.headerFields.append(contentsOf: additionalHeaderFields)
+            return try await next(req, body, baseURL)
+        }
+    }
+}
